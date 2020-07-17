6cd03a786d41c78b42b117620c849cedcc5b9624
Romy Maxwell <romy.maxwell@gmail.com>
2020-07-17 14:50:34 -0400

    Typo

--- background/background.js
+++ background/background.js
@@ -1,4 +1,4 @@
-const var GOOGLE_SPEECH_URI = 'https://www.google.com/speech-api/v1/synthesize';
+const GOOGLE_SPEECH_URI = 'https://www.google.com/speech-api/v1/synthesize';
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const { word, lang } = request, 
